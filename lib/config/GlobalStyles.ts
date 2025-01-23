4e6acbc440267f0fca7ea85258662133a84b6bff
Demis Rincon <darmfma@gmail.com>
2025-01-23 03:52:21 -0600

    fix to font

--- lib/config/GlobalStyles.ts
+++ lib/config/GlobalStyles.ts
@@ -7,7 +7,7 @@ const GlobalStyles = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        font-family: ${poppins};
+        font-family: ${poppins.style.fontFamily};
     }
  
     h1, h2, h3, h4, h5, h6 {
